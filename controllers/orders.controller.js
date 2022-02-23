0af820d0f862cd48a2f4d0c79b0774454bca7c82
Yashraj Wani <yashrajwani9@gmail.com>
2022-02-23 10:58:52 +0530

    Added payment with stripe API

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,11 +1,11 @@
-const Order = require('../models/order.model');
-const User = require('../models/user.model');
+const Order = require("../models/order.model");
+const User = require("../models/user.model");
 
 async function getOrders(req, res) {
   try {
     const orders = await Order.findAllForUser(res.locals.uid);
-    res.render('customer/orders/all-orders', {
-      orders: orders
+    res.render("customer/orders/all-orders", {
+      orders: orders,
     });
   } catch (error) {
     next(error);
@@ -33,10 +33,43 @@ async function addOrder(req, res, next) {
 
   req.session.cart = null;
 
-  res.redirect('/orders');
+  const STRIPE_KEY = ""; // add api key to test payments
+  const stripe = require("stripe")(STRIPE_KEY);
+  const YOUR_DOMAIN = "http://localhost:3000";
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: cart.items.map((item) => {
+      return {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price_data: {
+          currency: "inr",
+          product_data: { name: item.product.title },
+          unit_amount_decimal: +item.product.price.toFixed(2) * 100,
+        },
+        quantity: item.quantity,
+      };
+    }),
+    mode: "payment",
+    success_url: `${YOUR_DOMAIN}/orders/success`,
+    cancel_url: `${YOUR_DOMAIN}/orders/cancel`,
+  });
+
+  res.redirect(303, session.url);
+
+  // res.redirect("/orders");
+}
+
+function getSuccess(req, res) {
+  res.render("customer/orders/success");
+}
+
+function getFailure(req, res) {
+  res.render("customer/orders/failure");
 }
 
 module.exports = {
   addOrder: addOrder,
   getOrders: getOrders,
+  getFailure: getFailure,
+  getSuccess: getSuccess,
 };
--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -1,11 +1,13 @@
-const express = require('express');
+const express = require("express");
 
-const ordersController = require('../controllers/orders.controller');
+const ordersController = require("../controllers/orders.controller");
 
 const router = express.Router();
 
-router.post('/', ordersController.addOrder); // /orders
+router.post("/", ordersController.addOrder); // /orders
 
-router.get('/', ordersController.getOrders); // /orders
+router.get("/", ordersController.getOrders); // /orders
+router.get("/success", ordersController.getSuccess);
+router.get("/failure", ordersController.getFailure);
 
 module.exports = router;
